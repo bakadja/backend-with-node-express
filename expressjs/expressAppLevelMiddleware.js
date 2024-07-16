a6caf2dc2127ad97573ab34b45b6b23048db5b3e
ngongang <k.ngongang@addon-solution.de>
2024-07-16 14:52:39 +0200

    chore: Update express server port to 3334

--- expressjs/expressAppLevelMiddleware.js
+++ expressjs/expressAppLevelMiddleware.js
@@ -6,24 +6,28 @@ const app = new express();
 
 // Middleware to check for a specific password in query parameters
 app.use(function (req, res, next) {
+
     // Check if the password query parameter matches the expected value
     if (req.query.password !== "pwd123") {
         // Send an error response if the password does not match
         return res.status(402).send("This user cannot login ");
     }
+
     // Log the current time
     console.log('Time:', Date.now());
+
     // Call the next middleware function
     next();
 });
 
 // Define a route for the /home path
 app.get("/home", (req, res) => {
+
     // Send a "Hello World!" message as a response
     return res.send("Hello World!");
 });
 
 // Start the server and listen on port 3333
-app.listen(3333, () => {
-    console.log(`Listening at http://localhost:3333`);
+app.listen(3334, () => {
+    console.log(`Listening at http://localhost:3334`);
 });
